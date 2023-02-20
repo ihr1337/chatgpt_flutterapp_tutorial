d3bbb28169127ca6e6a13cb3c7b351e92a2bc713
captcha1337 <nissans2000@gmail.com>
2023-02-20 23:00:53 +0200

    wrote a comment about API key

--- lib/constants/api_consts.dart
+++ lib/constants/api_consts.dart
@@ -1,4 +1,5 @@
 // ignore_for_file: non_constant_identifier_names
 
 String BASE_URL = 'https://api.openai.com/v1';
-String API_KEY = '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';
+String API_KEY =
+    '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'; //! You put your OpenAI API here
